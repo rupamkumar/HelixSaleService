7f06177ce248a428d9b3132965c4d025e992654d
Rupam Sarkar <rupam.sarkar@primaryhealthcare.com.au>
2016-04-15 13:57:54 +0800

    this is test

--- DEV/2.PullDownFromBranchProjectA.bat
+++ DEV/2.PullDownFromBranchProjectA.bat
@@ -21,6 +21,7 @@ rem echo %SOLFOLDER%
 rem pull does not work of version 1.8.1.0
 rem %SOLFOLDER%\Utilities\git\git.exe fetch 
 rem echo %GPATH%
+%GPATH%git.exe pull --rebase
 %GPATH%git.exe pull origin : ProjectA
  IF NOT %ERRORLEVEL%== 0 GOTO :ERROREXIT
 
