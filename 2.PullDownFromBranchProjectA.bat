9b16e85d58720fb63831f89dbb2345ce3c74c8e6
Rupam Sarkar <rupam.sarkar@primaryhealthcare.com.au>
2016-04-18 09:26:56 +0800

    this is test

---
+++ 2.PullDownFromBranchProjectA.bat
@@ -0,0 +1,42 @@
+@Echo off
+SET BASEFOLDER=%~dp0..
+set PATH=%PATH%;C:\Program Files\Git\bin\
+set GPATH = C:\Program Files\Git\bin\
+
+
+
+
+
+rem echo %BASEFOLDER%
+rem SET SOLFOLDER=%BASEFOLDER%\..\..\..\
+
+SET SOLFOLDER=%BASEFOLDER%
+echo %SOLFOLDER%
+PUSHD .
+cd %SOLFOLDER%
+
+SET SOLFOLDER=%CD%\
+	POPD
+rem echo %SOLFOLDER%
+rem pull does not work of version 1.8.1.0
+rem %SOLFOLDER%\Utilities\git\git.exe fetch 
+rem echo %GPATH%
+rem %GPATH%git.exe pull --rebase  
+%GPATH%git.exe checkout HeilxLeisure
+%GPATH%git.exe pull origin : ProjectA
+ IF NOT %ERRORLEVEL%== 0 GOTO :ERROREXIT
+
+ GOTO EXIT 
+rem cd C://Projects/Sapphire/
+rem git pull
+:ERROREXIT
+ECHO.
+ECHO.
+	ECHO --------------------------------------------------------------
+	ECHO -------THERE WAS AN ISSUE, FETCH IS NOT COMPLETE------------
+	ECHO --------------------------------------------------------------
+ECHO.
+ECHO.	
+
+:EXIT
+pause
